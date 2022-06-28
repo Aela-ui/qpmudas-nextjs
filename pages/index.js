fa7fedacfa1ff1414274ff9bdd6bb2735541ba87
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2022-06-27 21:52:36 -0300

    feat(navbar): added a fancy navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import styled from 'styled-components'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Text>hi</Text>
     </div>
-  )
+  );
 }
 
 const Text = styled.h1`
